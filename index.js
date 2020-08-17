7ef7a98d8bd1b80f8327f8b93b597626116735c2
Sxmurai <gavrw0607@gmail.com>
2020-08-16 19:27:16 -0500

    removal of packages and updates in index.js

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const { auth, target, message, count } = require("./settings.json");
 
 (async () => {
   client.state.generateDevice(auth.username);
-  await client.account.login(auth.username, auth.password);
+  const account = await client.account.login(auth.username, auth.password);
+  console.log(
+    `[LOGS] | Logged in as ${account.username} (${account.pk}), time to send ${count} messages!`
+  );
 
   const user = (await client.direct.rankedRecipients()).ranked_recipients.find(
     (v) => v.thread.thread_title === target
   );
 
-  if (!user) throw new Error(`Invalid user.`);
+  if (!user)
+    return console.log(`[LOGS] | It seems you provided an invalid username.`);
 
   const id = user.thread.users[0].pk;
 
